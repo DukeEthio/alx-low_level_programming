7fda61f1564e48d2aafe6b315888c0f1adf9f24a
DukeEthio <dukeethiopian@gmail.com>
2022-09-15 14:18:53 -0700

    vi 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,6 +3,7 @@
  * mul - Multiples two integers.
  * @a: The first integer to be multiplied.
  * @b: The second integer to be multiplied.
+ *
  * Return: The result of the multiplication.
  */
 int mul(int a, int b)
