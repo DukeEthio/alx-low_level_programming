1c58438264c2757babcf82f4252e4c955b4d30e3
DukeEthio <dukeethiopian@gmail.com>
2022-09-15 12:55:59 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,15 +1,10 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN_H
 /**
- * File: main.h
- * Auth: Mesfin Mitikie
- * Desc: Header file containing prototypes for all functions used in the
- * 0x04-more_functions_nested_loops directory.
+ * main - header file for protoypes
+ * void protoyes (void)
+ * int protoypes (int)
  */
-int _putchar(char c);
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
@@ -18,5 +13,9 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
 
-#endif
+#endif /*MAIN_H*/
