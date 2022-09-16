cdf5c01fc88096a0ad23494d464581633bda7995
DukeEthio <dukeethiopian@gmail.com>
2022-09-16 14:26:41 -0700

    0-main.c  1-main.c  2-largest_number.c  3-print_remaining_days.c  main.h

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,11 +1,13 @@
 #include "main.h"
 /**
- * main - tests function that prints if integer is positive_or_negative
+ * main - test function for positive_or_negative
  * Return: 0
  */
 int main(void)
 {
-int i = 0;
-positive_or_negative(i);
-return (0);
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
 }
