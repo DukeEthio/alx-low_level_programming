3ed169eb52ce01e054aafcdbda1094fd6ebab0b4
DukeEthio <dukeethiopian@gmail.com>
2022-09-21 14:51:44 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -4,7 +4,7 @@
  * File: main.h
  * Auth: Mesfin Mitikie
  * Desc: Header file containing prototypes for all functions
- * used in the 0x06-pointer_arrys_strings directory
+ * used in the 0x05-pointers_arrays_strings directory.
  */
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
