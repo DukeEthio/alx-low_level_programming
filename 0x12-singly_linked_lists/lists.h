36edd5853311fc7329740759e02755065dc4b0e2
DukeEthio <dukeethiopian@gmail.com>
2022-10-20 08:49:48 -0700

    mandatory tasks

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,13 +1,16 @@
 #ifndef _LISTS_H_
 #define _LISTS_H_
+
 #include <stddef.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
  *
- * Desc: singly linked list node structure for ALX project
+ * Description: singly linked list node structure
+ * for Holberton project
  */
 typedef struct list_s
 {
@@ -23,4 +26,4 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif /* _LISTS_H_*/
+#endif /* _LISTS_H_ */
